fbf46d1d9f2ce27d544d966f0bf10e5d9143b57c
Kamil Jochym <kamil.jochym@warwick.ac.uk>
2022-12-02 10:32:48 +0100

    build

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
+app.use(express.static('build'))
+
 app.get('/', (req, res) => {
-  res.send('backend loll baby')
+  res.send('backend works')
 })
 
 app.get('/quote', (req, res) => {
@@ -21,7 +23,7 @@ app.get('/quote', (req, res) => {
     })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
